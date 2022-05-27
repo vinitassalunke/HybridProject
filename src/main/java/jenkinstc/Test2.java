0054d37694e106efc80548be9102018113301e47
Vinita <Vinita@DESKTOP-BON5OPP>
2022-05-27 20:23:33 +0530

    Hi

---
+++ src/main/java/jenkinstc/Test2.java
@@ -0,0 +1,12 @@
+package jenkinstc;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 {
+	@Test
+	public void tc3()
+	{
+		Reporter.log("Hi",true);
+	}
+}
---
+++ src/main/java/jenkinstc/Test3.java
@@ -0,0 +1,12 @@
+package jenkinstc;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 {
+	@Test
+	public void tc3()
+	{
+		Reporter.log("Hello python",true);
+	}
+}
